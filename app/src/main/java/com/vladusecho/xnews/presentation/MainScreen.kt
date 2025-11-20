6f55607e85af0cf8b7124b75634382bd4771c9af
Владислав <vladkorzun2016@yandex.ru>
2025-11-20 09:52:02 +0300

    feat(MainScreen): change search icon

--- app/src/main/java/com/vladusecho/xnews/presentation/MainScreen.kt
+++ app/src/main/java/com/vladusecho/xnews/presentation/MainScreen.kt
@@ -1,32 +1,26 @@
 package com.vladusecho.xnews.presentation
 
-import android.widget.Button
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -38,14 +32,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
+import com.vladusecho.xnews.R
 import com.vladusecho.xnews.data.models.ArticleDto
 import com.vladusecho.xnews.ui.theme.XNewsTheme
 
@@ -76,34 +71,27 @@ fun MainScreen() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.Bottom
             ) {
                 OutlinedTextField(
                     value = text,
                     onValueChange = { text = it },
                     label = { Text(text = "Введите запрос") }
                 )
-                Button(
-                    modifier = Modifier,
-                    onClick = {
-                        viewModel.loadArticles(text)
-                    },
-                    colors = ButtonDefaults.buttonColors().copy(
-                        contentColor = Color.Black,
-                        containerColor = Color.White
-                    ),
-                    border = BorderStroke(
-                        width = 1.dp,
-                        brush = Brush.linearGradient(
-                            colors = listOf(Color.Magenta, Color.Gray)
-                        )
-                    ),
-                    shape = RoundedCornerShape(5.dp)
-                ) {
-                    Text(
-                        text = "Найти"
-                    )
-                }
+
+                Icon(
+                    modifier = Modifier
+                        .size(58.dp)
+                        .clip(RoundedCornerShape(50))
+                        .clickable(
+                            enabled = true
+                        ) {
+                            viewModel.loadArticles(text)
+                        },
+                    contentDescription = null,
+                    painter = painterResource(R.drawable.ic_search),
+                    tint = Color.Unspecified
+                )
             }
             when (currentState) {
                 is MainState.Content -> {
@@ -147,10 +135,12 @@ fun MainScreen() {
                             hostState = snackbarHostState,
                             modifier = Modifier
                                 .align(Alignment.BottomCenter)
-                            .padding(11.dp)
+                                .padding(11.dp)
                         ) {
-                            Snackbar(it,
-                                contentColor = Color.Black, containerColor = Color.Cyan)
+                            Snackbar(
+                                it,
+                                contentColor = Color.Black, containerColor = Color.Cyan
+                            )
                         }
                         LaunchedEffect(showSnackbar) {
                             if (showSnackbar) {
